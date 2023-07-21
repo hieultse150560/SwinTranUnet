4138416de910b8c205937c286e242b72d90f589a
Hieu Le <62134254+hieultse150560@users.noreply.github.com>
2023-07-21 11:29:16 +0700

    Update generative.py

--- generative.py
+++ generative.py
@@ -44,50 +44,44 @@ def get_objects(response):
   response,_ = get_completion(prompt, history)
   return response
 
+def checkLength(string_to_dump):
+    string_to_dump = (string_to_dump.split("\n"))[:-1] 
+    print("Generate: ", len(string_to_dump))
+    return len(string_to_dump)
 
 string_to_dump = ""
-for i in tqdm(range(2000)):
-  try:
-    samples = ["A desk contains a pen and an apple", "A table contains a set of keys and a knife", "A vase lies in the table", "A silver spoon and a wristwatch rest on a wooden nightstand", "A pair of scissors and a ruler rest on a wooden desk next to an open notebook."]
-    samples_text = "\n".join(samples)
-    prompt = f"""
-Your task is to create a simple description of a image used for training a robot to grasp.
+while True:
+    i = 1
+    if checkLength(string_to_dump) > 10000:
+        with open(f"./data/batch_{i}.pkl", "wb") as f:
+            string = pickle.dump("\n".join(string_to_dump.split("\n")[:10000]))
+        string_to_dump = "\n".join(string_to_dump[10000:])
+        print(f"Writing to ./data/batch_{i}.pkl")
+        i += 1
+    try:
+        samples = ["A desk contains a pen and an apple", "A table contains a set of keys and a knife", "A vase lies on the table", "A silver spoon and a wristwatch rest on a wooden nightstand", "A pair of scissors and a ruler rest on a wooden desk next to an open notebook."]
+        samples_text = "\n".join(samples)
+        prompt = f"""
+You'll need to create a simple description of an image used for training a robot to grasp.
 
-The description have the chance of 80% to generate in the form of 'A <container> contains <object 1>, <object 2>,...'.
+The description has the chance of 80% to generate in the form of 'A <container> contains <object 1>, <object 2>,...'.
 The description must be in one sentence and based on the following samples.
 Example:```\n{samples_text}\n```
 
-The objects that can be grasped should be small objects such as pencils, watches, spoons, knives, scissors, keys, spoons,... The description should contain from 1-3 objects.
+The objects that can be grasped should be small objects such as pencils, watches, spoons, knives, scissors, keys, spoons,... The description should contain 1-3 objects.
 Some common containers are corner tables, cabinets, shelves, desks,...
 
-The description MUST BE DIVERSE AND NOT THE SAME AS THE GENERATED ONES.
+The description MUST BE DIVERSE AND NOT THE SAME AS THE GENERATED ONES. You can use synonyms of objects to increase diversity. For example, mug can be replaced with cup, bottle...
 """
-    history = []
-    for _ in range(10):
-      response, history = get_completion(prompt, history)
-      print(response, get_objects(response))
-      string_to_dump += response + " " + get_objects(response) + "\n"
-      # plot(response)
-    print(f"Done at {i}")
-    time.sleep(30)
-  except Exception as e:
-    print(e)
-    print(f"Fail at {i}")
-    time.sleep(30)
-string_to_dump = (string_to_dump.split("\n"))[:-1] 
-print("Generate: ", len(string_to_dump.split("\n")))
-
-with open("5kdescription.pkl", "rb") as f:
-    string = pickle.load(f)
-string += string_to_dump
-print("Current length: ", len(string.split("\n")))
-
-with open("5kdescription.pkl", "wb") as f:
-  pickle.dump(string, f)
-
-length = len(string.split("\n"))
-samples = string.split("\n")
-for i in range(length//10000):
-  with open(f"./data/batch_{i}.pkl", "wb") as f:
-    string = pickle.dump("\n".join(
-
+        history = []
+        for _ in range(10):
+          response, history = get_completion(prompt, history)
+          print(response, get_objects(response))
+          string_to_dump += response + " " + get_objects(response) + "\n"
+          # plot(response)
+        time.sleep(30)
+    except Exception as e:
+        print(e)
+        time.sleep(30)
+    string_to_dump = (string_to_dump.split("\n"))[:-1] 
+    print("Generated: ", len(string_to_dump.split("\n")))
