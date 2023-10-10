d2a80cf54ab4c4e0ff7f3d604072e0ffa479f45c
Hieu Le <62134254+hieultse150560@users.noreply.github.com>
2023-10-10 17:45:56 +0700

    Update generative2.py

--- generative2.py
+++ generative2.py
@@ -10,12 +10,12 @@ model = AutoModelForCausalLM.from_pretrained(
     rope_scaling={"type": "dynamic", "factor": 2} # allows handling of longer inputs
 )
 
-prompt = "### User:\nYour task is to create a simple description of an image.
+prompt = '''### User:\nYour task is to create a simple description of an image.
 The description must be simple, in one sentence length. The description should contain 1-3 objects separately placed on the container. Objects must be graspable, do not reuse generated objects in previously generated descriptions. The description only shows the name of objects, don't describe the objects too much.
 You can also use synonyms of objects to increase diversity, for example, mug, cup, and bottle can be considered synonymous.
 You must return only the description, nothing else. Don't include any explanation.
 
-\n\n### Assistant:\n"
+\n\n### Assistant:\n'''
 inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
 del inputs["token_type_ids"]
 streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
