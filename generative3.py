f11a99cbdae2bec64afe9ac2465908612283572e
Hieu Le <62134254+hieultse150560@users.noreply.github.com>
2024-01-19 10:13:18 +0700

    Create generative3.py

---
+++ generative3.py
@@ -0,0 +1,8 @@
+import subprocess
+
+command = "./main -m ./models/13B/ggml-model-q4_0.bin -p "Building a website can be done in 10 simple steps:\nStep 1:" -n 400 -e'"
+
+result = subprocess.run(command, shell=True, capture_output=True, text=True)
+output = result.stdout
+
+print(output)
