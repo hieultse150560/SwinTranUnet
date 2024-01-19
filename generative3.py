567e4655de0a1f2b698a04329222c5c47713af91
Hieu Le <62134254+hieultse150560@users.noreply.github.com>
2024-01-19 11:38:10 +0700

    Update generative3.py

--- generative3.py
+++ generative3.py
@@ -1,6 +1,6 @@
 import subprocess
 
-command = """./main -m ./models/13B/ggml-model-q4_0.bin --n-gpu-layers 15000 -p "Your task is to create a simple description of an image. The description must be simple, in one sentence length. The description should contain 1-3 objects separately placed on the container. Objects must be graspable, do not reuse generated objects in previously generated descriptions. The description only shows the name of objects, don't describe the objects too much. You can also use synonyms of objects to increase diversity, for example, mug, cup, and bottle can be considered synonymous. You must return only the description, nothing else. Don't include any explanation. Description:" -n 400 -e"""
+command = """./main -m ./models/13B/ggml-model-q4_0.bin --n-gpu-layers 15000 -p "Your task is to create a simple description of an image. The description must be simple, in one sentence length. The description should contain 1-3 objects separately placed on the container. Objects must be graspable, do not reuse generated objects in previously generated descriptions. The description only shows the name of objects, don't describe the objects too much. You can also use synonyms of objects to increase diversity, for example, mug, cup, and bottle can be considered synonymous. You must return only the description, nothing else. Don't include any explanation." -n 400 -e"""
 
 result = subprocess.run(command, shell=True, capture_output=True, text=True)
 output = result.stdout
