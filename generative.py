c0d0706547cbae4abd6186bb7a544642ef71ecfa
Hieu Le <62134254+hieultse150560@users.noreply.github.com>
2024-01-25 17:44:38 +0700

    Update generative.py

--- generative.py
+++ generative.py
@@ -1,47 +1,22 @@
-import openai
-import os
+import subprocess
+import re
 import time  
 import pickle
 from tqdm import tqdm
 
-os.environ["OPENAI_API_TYPE"] = "azure"
-os.environ["OPENAI_API_VERSION"] = "2023-03-15-preview"
-os.environ["OPENAI_API_BASE"] = "https://airesidency-grasp-anything.openai.azure.com/"
-os.environ["OPENAI_API_KEY"] = "2da25ecd69a040bf874bb6921b412606"
-openai.api_type = "azure"
-openai.api_base = "https://airesidency-grasp-anything.openai.azure.com/"
-openai.api_version = "2023-03-15-preview"
-openai.api_key = "2da25ecd69a040bf874bb6921b412606"
-
-def get_completion(prompt, history):
-    history.append({"role": "user", "content": prompt})
-    response = openai.ChatCompletion.create(engine = "gpt-35-turbo", messages=history, temperature = 1.0, top_p = 0.5, frequency_penalty = 1.0, presence_penalty = 1.0, stop = None)
-    message_response = response["choices"][0]["message"]["content"]
-    history.append({"role": "system", "content": message_response})
-    return message_response, history
+def get_completion():
+    command = """./main -m ./models/13B/ggml-model-q4_0.bin --n-gpu-layers 15000 -p "Your task is to create a simple description of an image. The description must be simple, in one sentence length. The description should contain 1-3 objects separately placed on the container. Objects must be graspable, do not reuse generated objects in previously generated descriptions. The description only shows the name of objects, don't describe the objects too much. You can also use synonyms of objects to increase diversity, for example, mug, cup, and bottle can be considered synonymous. You must return only the description, nothing else. Don't include any explanation.\nAnswer:" -n 400 -e --temp 1.0"""
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    output = result.stdout
+    description = re.findall(r"Answer:\s+(.*?)(?:\.|\]|$)", output, flags=re.IGNORECASE)
+    return description[0]
 
 def get_objects(response):
-  prompt = f"""
-Your task is to seperate the objects in a simple description of a image. 
-
-The objects that can be grasped should be small objects such as pencil, watch, spoon, knife, scissors, key, spoon,... The objects must be converted to singular and have 1 word length. For example, you must convert 'keys' to 'key'.
-
-Some common containers are corner table, cabinet, shelf, desk,... You should return only the objects, not the container. 
-
-The results should be in the form [<object 1>, <object 2>,...]. The response must be the list containing the objects, nothing else.
-
-Example:```A metal cabinet contains a pair of scissors and a set of keys -> [scissors, key]
-A table contains a set of keys and a knife -> [key, knife]
-A set of keys and a pair of sunglasses are lying on top of a wooden nightstand. -> [key, sunglasses]
-A red mug on a kitchen counter holds a teaspoon and a sugar packet. -> [teaspoon, packet]
-A wooden nightstand contains a phone and a pair of glasses. -> [phone, glasses]
-A glass jar containing a set of colored pencils is on top of a wooden table. -> [pencil]```
-
-Description: {response}
-Response: """
-  history = []
-  response,_ = get_completion(prompt, history)
-  return response
+    command = """./main -m ./models/13B/ggml-model-q4_0.bin --n-gpu-layers 15000 -p f""" Your task is to seperate the objects in a simple description of a image. The objects that can be grasped should be small objects such as pencil, watch, spoon, knife, scissors, key, spoon,... The objects must be converted to singular and have 1 word length. For example, you must convert 'keys' to 'key'. Some common containers are corner table, cabinet, shelf, desk,... You should return only the objects, not the container. The results should be in the form [<object 1>, <object 2>,...]. The response must be the list containing the objects, nothing else. Example:A metal cabinet contains a pair of scissors and a set of keys -> [scissors, key] A table contains a set of keys and a knife -> [key, knife] A set of keys and a pair of sunglasses are lying on top of a wooden nightstand. -> [key, sunglasses] A red mug on a kitchen counter holds a teaspoon and a sugar packet. -> [teaspoon, packet] A wooden nightstand contains a phone and a pair of glasses. -> [phone, glasses] A glass jar containing a set of colored pencils is on top of a wooden table. -> [pencil] Description: {description}  Response: """ -n 400 -e
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    output = result.stdout
+    objects = re.findall(r"Response:\s+(.*?)(?:\.|\]|$)", output, flags=re.IGNORECASE)
+    return objects[0]
 
 def checkLength(string_to_dump):
     string_to_dump = (string_to_dump.split("\n"))[:-1] 
@@ -50,7 +25,7 @@ def checkLength(string_to_dump):
 
 k = 10000
 string_to_dump = ""
-i = 48
+i = 68
 while True:
     if checkLength(string_to_dump) > k:
         with open(f"./data/batch_{i}.pkl", "wb") as f:
@@ -60,26 +35,11 @@ while True:
         i += 1
         print("Left: ", checkLength(string_to_dump))
     try:
-        samples = ["A desk contains a pen and an apple", "A table contains a set of keys and a knife", "A vase lies on the table", "A silver spoon and a wristwatch rest on a wooden nightstand", "A pair of scissors and a ruler rest on a wooden desk next to an open notebook."]
-        samples_text = "\n".join(samples)
-        prompt = f"""
-Your task is to create a simple description of an image used for training a robot to grasp.
-
-The description must be in one sentence. The description should contain 1-3 objects. You must use all small objects that can be grasped with the same probability, except the objects in previously generated descriptions. 
-
-The description MUST BE DIVERSE AND NOT THE SAME STRUCTURE AS THE GENERATED ONES. You can also use synonyms of objects to increase diversity, for example, mug, cup, and bottle can be considered synonymous. 
-
-You must return only the description, nothing else. 
-"""
-        history = []
-        for _ in range(15):
-          response, history = get_completion(prompt, history)
-          print(response, get_objects(response))
-          string_to_dump += response + " " + get_objects(response) + "\n"
-          # plot(response)
-        time.sleep(15)
+        response = get_completion()
+        objects = get_objects(response)
+        print(response, objects)
+        string_to_dump += response + " " + objects + "\n"
     except Exception as e:
         print(e)
-        time.sleep(15)
     new = (string_to_dump.split("\n"))[:-1] 
     print("Generated: ", len(new), "\n")
